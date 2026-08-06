43371898091ebd7de37b5c6e9a62f87d4a0f5f2d
Sreedhar-1305 <sreedharreddy2005@gmail.com>
2026-08-06 11:45:16 +0530

    Day4

--- track/M02-introjava/dowhile.java
+++ track/M02-introjava/dowhile.java
@@ -1,14 +1,14 @@
-public class whileloop {
+public class dowhile {
     public static void main(String[] args) {
         int i = 1;
-        while (i <= 5) {
+        do {
             int j = 1;
-            while (j <= 5) {
+            do {
                 System.out.println(j);
                 j++;
-            }
+            } while (j <= 5);
             System.out.println();
             i++;
-        }
+        } while (i <= 5);
     }
 }
