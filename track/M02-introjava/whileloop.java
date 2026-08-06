e4a482ba581781a3eb259bb516c600c51fc7fbdd
Sreedhar-1305 <sreedharreddy2005@gmail.com>
2026-08-06 11:35:16 +0530

    Day4

---
+++ track/M02-introjava/whileloop.java
@@ -0,0 +1,13 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
