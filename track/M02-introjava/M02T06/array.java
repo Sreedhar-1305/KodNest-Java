79ecbf56be15b06ab5cc2e472ed87d250d2d2875
Sreedhar-1305 <sreedharreddy2005@gmail.com>
2026-08-17 12:28:39 +0530

    Week2

--- track/M02-introjava/M02T06/array.java
+++ track/M02-introjava/M02T06/array.java
@@ -1,7 +1,17 @@
 package M02T06;
 
+import java.util.*;
+
 public class array {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Arrays");
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
